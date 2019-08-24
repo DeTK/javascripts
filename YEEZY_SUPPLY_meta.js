faca8f94007895b8bfd64a8b8c86368ce4f4ac36
DeTK <sda2225@gmail.com>
2019-08-25 03:57:18 +0900

    샘플 추가 샘플을 추가했다

---
+++ YEEZY_SUPPLY_meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         YEEZY SUPPLY
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       DeTK
+// @match        https://yeezysupply.com
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/DeTK/javascripts/master/YEEZY_SUPPLY_meta.js
+// @downloadURL  https://raw.githubusercontent.com/DeTK/javascripts/master/YEEZY_SUPPLY_user.js
+// @require      https://raw.githubusercontent.com/DeTK/javascripts/master/sleep.js
+// ==/UserScript==
