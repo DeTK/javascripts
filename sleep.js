2629d9f1875e30ee099c2f6e039d5dde4b143323
DeTK <45531377+DeTK@users.noreply.github.com>
2019-08-25 15:36:52 +0900

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,4 +1,5 @@
-const sleep = this.prototype.time = function(sec = 0) {
+const sleep = function(){}
+sleep.prototype.time = function(sec = 0) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(` ${this.sec / 1000} 초`);
