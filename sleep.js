ce51c5321298d3ba3bcea69855c7dd23f4ef9257
DeTK <45531377+DeTK@users.noreply.github.com>
2019-08-25 14:20:36 +0900

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,7 +1,7 @@
-function sleep (sec = 0){
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`성공1`);
-    },sec);
-  });
+function sleep (sec) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("성공12314");
+        }, sec);
+    });
 }
