e0d5b1f46910bf8020e1249b359c9c99ec67dfd5
DeTK <45531377+DeTK@users.noreply.github.com>
2019-08-25 14:40:34 +0900

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,7 +1,9 @@
 function sleep (sec) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(` ${sec / 1000} 초`);
-        }, sec);
-    });
+    return {
+        time : new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(` ${sec / 1000} 초`);
+            }, sec);
+        });
+        consoles : function(){console.log(sec)}
 }
