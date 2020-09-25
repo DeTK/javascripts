125dba9c380e17bef05a0e486296c8572f995a15
DeTK <45531377+DeTK@users.noreply.github.com>
2020-09-26 04:29:06 +0900

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,12 +1,13 @@
-var sleep = {
-    if(false === (this instanceof Object)){
-        return new sleep();
+function sleep()
+{
+    if(false === (this instanceof sleep)){
+        return {
+            time : function(sec = 0) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve();
+                }, sec);
+            }).then();
+        }
     }
-    time : function(sec = 0) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve();
-            }, sec);
-        }).then();
-    },
 }
