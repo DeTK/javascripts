206a75b0e871ce2a8e93204e9d9347f2736196d0
DeTK <45531377+DeTK@users.noreply.github.com>
2020-09-26 02:18:10 +0900

    Update Cookie.user.js

--- Cookie.user.js
+++ Cookie.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         쿠키
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  쿠키
 // @author       You
 // @connect      *
