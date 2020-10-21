5465733baa2256a0c2bc7f4dc3e90b56504b9042
DeTK <45531377+DeTK@users.noreply.github.com>
2020-10-22 01:32:28 +0900

    Update Cookie.meta.js

--- Cookie.meta.js
+++ Cookie.meta.js
@@ -1,4 +1,1 @@
-// ==UserScript==
 // @version      1
-// @updateURL    https://github.com/DeTK/javascripts/raw/master/Cookie.meta.js
-// @downloadURL  https://github.com/DeTK/javascripts/raw/master/Cookie.user.js
