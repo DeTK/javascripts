22311540579f1d01da72f077994aa9d629d6f339
DeTK <45531377+DeTK@users.noreply.github.com>
2021-07-29 21:11:21 +0900

    sleep object!
    my sleep solution

--- sleep.js
+++ sleep.js
@@ -2,5 +2,5 @@ const sleep = (() => {
     const time = (sec = 1) => new Promise((resolve) => setTimeout(()=>{console.log(`${delay / 1000} s`), resolve()}, delay));
     const element = async (query, sec = 1) => { while (!document.querySelector(query)) { await time(sec); } console.log(`> ${query}!`); };
     const random = async (min, max, baseTime) => { await time((Math.floor(Math.random() * (max- min + 1)) + min) * baseTime);};
-    return { Time: time, Element: element, Random: random };
+    return { Time: time, Element: element, Random: random }; 
 })();
