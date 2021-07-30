a99ca8351d96c54f4cdab09d5035b5675baa41fe
DeTK <45531377+DeTK@users.noreply.github.com>
2021-07-30 14:18:59 +0900

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,6 +1,6 @@
-const sleep = (() => {
+const sleep = () => {
     const time = (delay = 1) => new Promise((resolve) => setTimeout(()=>{console.log(`${delay / 1000} s`), resolve()}, delay));
     const element = async (query, delay = 1) => { while (!document.querySelector(query)) { await time(delay); } console.log(`> ${query}!`); };
     const random = async (min, max, baseTime) => { await time((Math.floor(Math.random() * (max- min + 1)) + min) * baseTime);};
     return { Time: time, Element: element, Random: random }; 
-})();
+};
